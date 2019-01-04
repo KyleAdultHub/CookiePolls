b5c0aa5c01326ca748bd4e43d37dfc4be2e6fe09
issacLiuUp <liukaijian45@163.com>
2019-01-04 19:54:08 +0800

    change

--- cookiespool/tester.py
+++ cookiespool/tester.py
@@ -1,89 +1,49 @@
 import json
-from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import ConnectionError
 from cookiespool.db import *
-from cookiespool.generator import WeiboCookiesGenerator
 
 
 class ValidTester(object):
-    def __init__(self, name='default'):
-        self.name = name
-        self.cookies_db = CookiesRedisClient(name=self.name)
-        self.account_db = AccountRedisClient(name=self.name)
-
-    def test(self, account, cookies):
+    def __init__(self, website='default'):
+        self.website = website
+        self.cookies_db = RedisClient('cookies', self.website)
+        self.accounts_db = RedisClient('accounts', self.website)
+    
+    def test(self, username, cookies):
         raise NotImplementedError
-
+    
     def run(self):
-        accounts = self.cookies_db.all()
-        for account in accounts:
-            username = account.get('username')
-            cookies = self.cookies_db.get(username)
-            self.test(account, cookies)
+        cookies_groups = self.cookies_db.all()
+        for username, cookies in cookies_groups.items():
+            self.test(username, cookies)
 
 
 class WeiboValidTester(ValidTester):
-    def __init__(self, name='weibo'):
-        ValidTester.__init__(self, name)
-
-    def test(self, account, cookies):
-        print('Testing Account', account.get('username'))
+    def __init__(self, website='weibo'):
+        ValidTester.__init__(self, website)
+    
+    def test(self, username, cookies):
+        print('正在测试Cookies', '用户名', username)
         try:
             cookies = json.loads(cookies)
         except TypeError:
-            # Cookie 格式不正确
-            print('Invalid Cookies Value', account.get('username'))
-            self.cookies_db.delete(account.get('username'))
-            print('Deleted User', account.get('username'))
-            return None
+            print('Cookies不合法', username)
+            self.cookies_db.delete(username)
+            print('删除Cookies', username)
+            return
         try:
-            response = requests.get('http://weibo.cn', cookies=cookies)
-            if response.status_code == 200:
-                html = response.text
-                soup = BeautifulSoup(html, 'lxml')
-                title = soup.title.string
-                if title == '我的首页':
-                    print('Valid Cookies', account.get('username'))
-                else:
-                    print('Title is', title)
-                    # Cookie已失效
-                    print('Invalid Cookies', account.get('username'))
-                    self.cookies_db.delete(account.get('username'))
-                    print('Deleted User', account.get('username'))
-        except ConnectionError as e:
-            print('Error', e.args)
-            print('Invalid Cookies', account.get('username'))
-
-
-class MWeiboValidTester(ValidTester):
-    def __init__(self, name='weibo'):
-        ValidTester.__init__(self, name)
-
-    def test(self, account, cookies):
-        print('Testing Account', account.get('username'))
-        try:
-            cookies = json.loads(cookies)
-        except TypeError:
-            # Cookie 格式不正确
-            print('Invalid Cookies Value', account.get('username'))
-            self.cookies_db.delete(account.get('username'))
-            print('Deleted User', account.get('username'))
-            return None
-        try:
-            test_url = 'http://m.weibo.cn/api/container/getIndex?uid=1804544030&type=uid&page=1&containerid=1076031804544030'
+            test_url = TEST_URL_MAP[self.website]
             response = requests.get(test_url, cookies=cookies, timeout=5, allow_redirects=False)
             if response.status_code == 200:
-                print('Valid Cookies', account.get('username'))
+                print('Cookies有效', username)
             else:
                 print(response.status_code, response.headers)
-                print('Invalid Cookies', account.get('username'))
-                self.cookies_db.delete(account.get('username'))
-                print('Deleted User', account.get('username'))
+                print('Cookies失效', username)
+                self.cookies_db.delete(username)
+                print('删除Cookies', username)
         except ConnectionError as e:
-            print('Error', e.args)
-            print('Invalid Cookies', account.get('username'))
+            print('发生异常', e.args)
 
 if __name__ == '__main__':
-    tester = WeiboValidTester()
-    tester.run()
+    WeiboValidTester().run()
